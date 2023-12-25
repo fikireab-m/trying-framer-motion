8cd177c036fef5ec0376df7ee1fa590327c1a6de
fikireab-m <fikireab.meku@gmail.com>
2023-12-25 15:11:10 +0300

    slow exit

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,12 @@ export default function App() {
       </nav>
       <main>
         <AnimatePresence>
-          <motion.div
+          <motion.div className="main_view"
             key={selectedTab ? selectedTab.label : "empty"}
-            initial={{ y: 2000, opacity: 0 }}
+            initial={{ y: 2000, opacity: 0.8 }}
             animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 2000, opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            exit={{ y: 2000, opacity: 0, decelerate:true }}
+            transition={{ duration: 1.0}}
           >
             {selectedTab ? selectedTab.icon : "😋"}
           </motion.div>
